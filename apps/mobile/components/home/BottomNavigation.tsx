95e4d90e5031d3d0dd60215f0284cd38be2d2f26
Rithvik086 <145756002+Rithvik086@users.noreply.github.com>
2026-02-17 22:45:33 +0530

    feat:added camera feature

--- apps/mobile/components/home/BottomNavigation.tsx
+++ apps/mobile/components/home/BottomNavigation.tsx
@@ -1,7 +1,7 @@
 import { View, TouchableOpacity, Text } from "react-native";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-
+import { router, usePathname } from "expo-router";
 interface NavItem {
   id: string;
   label: string;
@@ -18,24 +18,32 @@ const NAV_ITEMS: NavItem[] = [
 
 export function BottomNavigation() {
   const insets = useSafeAreaInsets();
+  const pathname = usePathname();
 
   return (
     <View
       style={{ paddingBottom: insets.bottom }}
-      className="fixed bottom-0 left-0 right-0 flex-row items-center border-t border-gray-100 bg-white"
+      className="absolute bottom-0 left-0 right-0 flex-row items-center border-t border-gray-100 bg-white"
     >
       {NAV_ITEMS.map((item, index) => (
         <TouchableOpacity
           key={item.id}
-          className={`flex-1 items-center justify-center py-4 ${
-            item.id === "camera" ? "mb-8" : ""
+          className={`flex-1 items-center justify-center py-2 ${
+            item.id === "camera" ? "mb-4" : ""
           }`}
+          onPress={() => {
+            if (item.id === "home") router.push("/(home)");
+            // if (item.id === "categories") router.push("/categories");
+            if (item.id === "camera") router.push("/(camera)");
+            // if (item.id === "wishlist") router.push("/wishlist");
+            // if (item.id === "profile") router.push("/profile");
+          }}
         >
           {item.id === "camera" ? (
-            <View className="mb-2 h-14 w-14 items-center justify-center rounded-full bg-primary shadow-lg">
+            <View className="mb-1 h-12 w-12 items-center justify-center rounded-full bg-primary shadow-lg">
               <MaterialCommunityIcons
                 name={item.icon}
-                size={24}
+                size={20}
                 color="white"
               />
             </View>
@@ -43,11 +51,11 @@ export function BottomNavigation() {
             <>
               <MaterialCommunityIcons
                 name={item.icon}
-                size={24}
+                size={20}
                 color={item.id === "home" ? "#b08d55" : "#a0aec0"}
               />
               <Text
-                className={`mt-1 text-[0.6rem] font-medium ${
+                className={`mt-1 text-[0.55rem] font-medium ${
                   item.id === "home" ? "text-primary" : "text-gray-400"
                 }`}
               >
